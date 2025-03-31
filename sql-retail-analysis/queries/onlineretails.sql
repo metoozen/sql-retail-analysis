2e6741e26a3534f3e0ef7ea3081f218be3d0699a
metoozen <72761010+metoozen@users.noreply.github.com>
2025-04-01 00:40:23 +0300

    Update onlineretails.sql

--- sql-retail-analysis/queries/onlineretails.sql
+++ sql-retail-analysis/queries/onlineretails.sql
@@ -1,29 +1,29 @@
-#Boş kayıtları çıkartıyorum
+--Boş kayıtları çıkartıyorum
 DELETE FROM onlineretails.onlineretail
 WHERE CustomerID IS NULL;
 
-#Quantity 0'dan düşük olanlar iade anlamına geliyor onları ayrı tabloya taşıyacağım
+--Quantity 0'dan düşük olanlar iade anlamına geliyor onları ayrı tabloya taşıyacağım
 CREATE TABLE returned_sales AS
 SELECT * FROM onlineretails.onlineretail
 WHERE quantity <= 0;
 
-#Mevcut tablodan iadeleri sileceğim
+--Mevcut tablodan iadeleri sileceğim
 DELETE FROM onlineretails.onlineretail
 WHERE quantity <= 0;
 
-#Formatı timestamp yapıyorum
+--Formatı timestamp yapıyorum
 ALTER TABLE onlineretails.onlineretail ADD COLUMN InvoiceTimestamp TIMESTAMP;
 
 UPDATE onlineretails.onlineretail
 SET InvoiceTimestamp = STR_TO_DATE(InvoiceDate, '%d/%m/%Y %H:%i');
 
-#Toplam satış sütunu
+--Toplam satış sütunu
 ALTER TABLE onlineretails.onlineretail ADD COLUMN TotalPrice DECIMAL(10,2);
 
 UPDATE onlineretails.onlineretail
 SET TotalPrice = Quantity * UnitPrice;
 
-
+-- Tablo yaratımı
 CREATE TABLE transactions (
     invoice_no TEXT,
     stock_code TEXT,
